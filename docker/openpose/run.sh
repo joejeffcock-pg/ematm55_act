10f2c993ad7c69cbe3629f5aeba6f154a8f73e01
Joe Jeffcock <tp21629@bristol.ac.uk>
2022-08-23 18:10:24 +0100

    DIS-41 chore: docker application output dir arg

--- docker/openpose/run.sh
+++ docker/openpose/run.sh
@@ -1,18 +1,27 @@
-if [ -z "$1" ]
+if [ "$#" -eq 0 ]
     then
-        echo "usage: ./run.sh <input directory>"
+        echo "usage: ./run.sh <input directory> [output directory]"
 else
+    # set output directory
+    output_dir=$PWD/outputs
+    if [ "$#" -eq 2 ]
+        then
+            output_dir="$2"
+    fi
+
+    # run OpenPose container mounting input/output/scripts
+    # before executing pose extraction
     nvidia-docker run \
         -it \
         --rm \
         --gpus=all \
         --env=PYTHONPATH=/root/openpose/build/python/openpose \
         --volume $1:/root/inputs \
-        --volume $PWD/outputs:/root/outputs \
-        --volume $PWD/extract_poses.py:/root/find_videos.py \
+        --volume $output_dir:/root/outputs \
+        --volume $PWD/extract_poses.py:/root/extract_poses.py \
         --env=DISPLAY \
         --env=QT_X11_NO_MITSHM=1 \
         --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw \
         joejeffcockpg/ematm55_openpose:latest \
-        python3 /root/find_videos.py
+        python3 /root/extract_poses.py
 fi
